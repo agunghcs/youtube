d78a8cff5edfd3d7c3030684cd03eb3e254b1b9f
mamasagung <65334552+mamasagung@users.noreply.github.com>
2023-10-05 10:13:01 +0700

    Add files via upload

---
+++ yt.py
@@ -0,0 +1,42 @@
+from pytube import YouTube
+from time import sleep
+
+
+
+def printLow(Str):
+    for char in Str:
+        print(char, end='', flush=True)
+        sleep(.01)
+        
+        
+r='\033[1;31m' 
+g='\033[32;1m' 
+y='\033[1;33m'
+w='\033[1;37m' 
+
+
+
+printLow(f"""\n{y}@url = """)
+    
+   
+link = input("")
+kir = YouTube(link)
+
+printLow(f"""\n{y}Info:
+    {g}[+] {y}nama video: {w}({kir.title})
+    {g}[+] {y}Penayangan video: {w}({kir.views})
+    {g}[+] {y}Durasi video (sec): {w}({kir.length})
+""")
+
+
+
+x = input(f'\n{g}[?] {r}anda ingin mengunduh(Y/n) {y}- {w}')
+if x.lower() == "y" :
+    printLow(f"\n{g}[+] {y}memuat ...\n")
+    ys = kir.streams.get_highest_resolution()
+    ys.download()
+    printLow(f"{g}[+] {y}selesai!\n")
+elif x.lower() == "n" :
+    printLow(f"\n{g}[-] {r}جار?. !\n")
+else:
+    printLow(f"{r}kesalahanأ!\n")
