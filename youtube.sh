e2ec6d2dc981d35b983fcdf0c714fa534ddb761c
mamasagung <65334552+mamasagung@users.noreply.github.com>
2023-11-02 09:47:53 +0700

    Add files via upload

---
+++ youtube.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+echo "1.> mp3 pytube"
+echo "2.> mp4 pytube /sdcard/ "
+echo "3.> exit "
+# membaca
+echo
+read -p [root@T00Ls-HACKER11:~# bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+pkg update && pkg upgrade
+pkg install figlet
+pip install lolcat
+pkg install python
+pkg install git
+pip install pytube
+git clone https://github.com/mamasagung/mp3-pytube
+cd mp3-pytube
+python mp3.py
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+pkg update && pkg upgrade
+pkg install git
+pkg install figlet
+pip install lolcat
+pkg install python
+pip install pytube
+git clone https://github.com/mamasagung/youtube
+cd youtube
+python yt3.py
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+exit
+fi
